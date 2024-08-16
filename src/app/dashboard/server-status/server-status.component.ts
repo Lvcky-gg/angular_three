276922aa2752cbc013e7680b4ddf1ff67d0bd2dc
lvcky-gg <matadorcrab@gmail.com>
2024-08-16 15:35:51 -0400

    n/a

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class ServerStatusComponent {
   currentStatus = 'online';
 
-  constructor() {
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
 
